95c0477031de82e3b8971a2e813f760164d11a7d
Кариша <lucaslima2458@gmail.com>
2022-11-29 22:31:53 -0300

    chore: 09 resolvido

--- 09/index.js
+++ 09/index.js
@@ -1,1 +1,13 @@
-const nome = 'Guido Cerqueira';
+const nome = 'Guido Cerqueira Júnior'.replace(/\s/g,"");
+
+const prefixo = "@";
+const menorOuIgual = `${prefixo}${nome.toLowerCase()}`;
+const maior = `${prefixo}${nome.slice(0, 12).toLowerCase()}`
+
+if (nome.length <= 13) {
+    console.log(menorOuIgual);
+}
+
+else {
+    console.log(maior)
+}
