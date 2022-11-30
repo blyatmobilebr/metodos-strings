8798cada15b244402840c9156d57f4a2bc828993
Кариша <lucaslima2458@gmail.com>
2022-11-29 22:29:41 -0300

    chore: 01 resolvido

--- 01/index.js
+++ 01/index.js
@@ -1,1 +1,8 @@
 const comentario = "Esse COVID é muito perigoso!";
+const censura = ["covid", "covid".toUpperCase(), "pandemia", "pandemia".toUpperCase()];
+
+for (let palavra of censura) {
+    if (comentario.includes(palavra)) console.log("Comentário bloqueado por conter palavras proibidas.");
+
+    // seria legal checar cada letra ... :eyes:
+}
