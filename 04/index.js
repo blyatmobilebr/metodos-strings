8555584efb27dd51276abf6fa3e6f61e784f8346
Кариша <lucaslima2458@gmail.com>
2022-11-29 22:31:29 -0300

    chore: 04 resolvido

--- 04/index.js
+++ 04/index.js
@@ -1,3 +1,13 @@
-let identificador = "123";
+let identificador = "1";
 let nome = "José silva costa";
 let email = "      jose@email.com  ";
+
+if(identificador.length < 6) console.log(identificador.padStart(6, "0"));
+
+
+let nomeArray = nome.split(" ").map(palavra => palavra.charAt().toUpperCase() ? palavra.charAt().toUpperCase() + palavra.slice(1) : -1);
+
+console.log(nomeArray.join(" "))
+
+
+console.log(email.trimStart());
