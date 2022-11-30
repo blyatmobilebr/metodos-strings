0b09b0cc544eb8d2f03fe583d34ea72e17c5e2da
Кариша <lucaslima2458@gmail.com>
2022-11-29 22:31:39 -0300

    chore: 06 resolvido

--- 06/index.js
+++ 06/index.js
@@ -1,1 +1,35 @@
 const celular = 7199918888;
+let ddd = "", prefixo = "";
+let primeirosNumeros = "", ultimosNumeros = "";
+
+
+function validar(numero) {
+    const texto = numero.toString();
+
+    if (texto.length === 11) {
+        ddd = `(${texto.slice(0,2)})`;
+        prefixo = texto.slice(2,3);
+        primeirosNumeros = texto.slice(3,7);
+        ultimosNumeros = texto.slice(7, 12)
+        return `${ddd} ${prefixo} ${primeirosNumeros}-${ultimosNumeros}`;
+
+    } 
+    
+    else if (texto.length === 10) {
+        ddd = texto.slice(0,2);
+        primeirosNumeros = texto.slice(2,6);
+        ultimosNumeros = texto.slice(6, 10)
+        return`(${ddd}) 9 ${primeirosNumeros}-${ultimosNumeros}`;
+    }
+
+    else if (texto.length === 8) {
+        prefixo = 9;
+        primeirosNumeros = texto.slice(0,4);
+        ultimosNumeros = texto.slice(4,9);
+
+        return `${prefixo} ${primeirosNumeros}-${ultimosNumeros}`;
+    }
+}
+
+
+console.log(validar(44444444));
