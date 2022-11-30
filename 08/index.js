00db9677c2850366cb04f976fef61b933a8a2ab6
Кариша <lucaslima2458@gmail.com>
2022-11-29 22:31:49 -0300

    chore: 08 resolvido

--- 08/index.js
+++ 08/index.js
@@ -1,1 +1,14 @@
 const cpf = "011.022.033-44";
+
+function tirarSujeira(numero) {
+    if (typeof numero === "string") return numero.replace(/\W+/g, "")
+    else if (typeof numero === "number") { 
+        numero = numero.toString();
+        return numero.replace(/\W+/g, "");
+
+        // iria checar pra BigInt, mas.... sem tempo
+    }
+
+}
+
+console.log(tirarSujeira(9348349-363))
