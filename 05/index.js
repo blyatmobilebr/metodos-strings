606735a94f9deab65e9d09bc4a672a0b69dc52e8
Кариша <lucaslima2458@gmail.com>
2022-11-29 22:31:34 -0300

    chore: 05 resolvido

--- 05/index.js
+++ 05/index.js
@@ -1,1 +1,7 @@
 const numeroCartao = '1111222233334444';
+
+const primeirosNumeros = numeroCartao.slice(0,4);
+const ultimosNumeros = numeroCartao.slice(12,16);
+const resto = numeroCartao.slice(4, 11).length
+
+console.log(`${primeirosNumeros}${"*".repeat(resto)}${ultimosNumeros}`)
