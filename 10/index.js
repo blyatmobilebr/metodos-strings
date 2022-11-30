e887b27d7b6d3fe240400e04e5fd4663d5e0e246
Кариша <lucaslima2458@gmail.com>
2022-11-29 22:32:00 -0300

    chore: 10 resolvido

--- 10/index.js
+++ 10/index.js
@@ -1,1 +1,9 @@
-const nomeArquivo = 'Foto da Familia.pdf';
+const nomeArquivo = 'Foto da Familia.png';
+
+if (nomeArquivo.endsWith(".jpg") || nomeArquivo.endsWith(".jpeg") || nomeArquivo.endsWith(".png") || nomeArquivo.endsWith(".gif")) {
+    console.log("Arquivo Válido");
+}
+
+else {
+    console.log("Arquivo Inválido");
+}
